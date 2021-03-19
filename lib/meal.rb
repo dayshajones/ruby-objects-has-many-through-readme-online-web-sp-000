3d60576e71dfcd4c0680aebdf4725c485b8a0e05
Daysha Jones <daysha.vu@icloud.com>
2021-03-19 18:46:29 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -3,7 +3,10 @@ class Meal
   attr_accessor :waiter, :customer, :total, :tip
   
   def initialize
-    
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
   end
   
 end
