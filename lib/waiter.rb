f2be79da60be792e621e9fd25d08f84695577548
Daysha Jones <daysha.vu@icloud.com>
2021-03-19 18:45:02 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -2,6 +2,8 @@ class Waiter
   
   attr_accessor :name, :years_of_experience
   
+  @@all = []
+  
   def initialize(name, years_of_experience)
     @name = name
     @years_of_experience = years_of_experience
